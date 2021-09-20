ca346b44bed1ab95918544ef619cd0d267cfdeda
iXavy <ict@ixavy.com>
2021-09-21 00:04:53 +0200

    Importar librerías

--- Modulo 1 - Crear una Cadena de Bloques/blockchain.py
+++ Modulo 1 - Crear una Cadena de Bloques/blockchain.py
@@ -7,3 +7,18 @@ Created on Mon Sep 20 23:35:11 2021
 """
 
 # Módulo 1 - Crear una Cadena de Bloques
+
+# Para Instalar:
+# Flask==1.1.2: pip install Flask==1.1.2
+# Cliente HTTP Postman: https://www.getpostman.com/
+
+# Importar las liberías
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Parte 1 - Crear la Cadena de Bloques
+
+
+# Parte 2 - Minado de un Bloque de la Cadena
